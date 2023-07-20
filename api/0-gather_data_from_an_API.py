9da05a63bba4a580d8871a6d868615d92bd28934
root <Caramonh@yahoo.com>
2023-07-20 13:14:46 -0700

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,32 +3,25 @@
 API set up to retrieve data from a url
 """
 import requests
+import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    url = f"{base_url}{employee_id}"
+if __name__ == '__main__':
+    API_URL = 'https://jsonplaceholder.typicode.com'
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        
-        employee_name = data["name"]
-        done_tasks = data["completed_tasks"]
-        total_tasks = data["total_tasks"]
-        completed_task_titles = [task["title"] for task in data["tasks"] if task["completed"]]
+    id = sys.argv[1]
+    request = requests.get('{}/users/{}/todos'.format(
+        API_URL, id), params={"_expand": "user"})
 
-        print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-        for title in completed_task_titles:
-            print("\t" + title)
+    response = request.json()
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: Failed to fetch data from the API. {e}")
+    completed_tasks = [task for task in response if task['completed']]
+    EMPLOYEE_NAME = response[0]['user']['name']
+    NUMBER_OF_DONE_TASKS = len(completed_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(response)
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py employee_id")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+    ))
+
+    for task in completed_tasks:
+        print("\t {}".format(task['title']))
