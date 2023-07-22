3ab12fbf96183329949f198ae4bd2edfac8e595d
root <Caramonh@yahoo.com>
2023-07-22 16:56:47 -0700

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""
-API set up to retrieve data from a url
-"""
-import json
+"""API set up to retrieve data from a url"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    API_URL = 'https://jsonplaceholder.typicode.com'
 
-    id = sys.argv[1]
-    request = requests.get('{}/users/{}/todos'.format(
-        API_URL, id), params={"_expand": "user"})
+def gather_api_data():
+    """gather and print api data"""
+    if(len(sys.argv) != 2):
+        print("Error not 3 commands")
 
-    response = request.json()
+    employee_id = sys.argv[1]
 
-    completed_tasks = [task for task in response if task['completed']]
-    EMPLOYEE_NAME = response[0]['user']['name']
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-    TOTAL_NUMBER_OF_TASKS = len(response)
+    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(employee_id)).json()
+    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params={"userId": employee_id}).json()
+    EMPLOYEE_NAME = user_data.get("name")
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-    ))
+    completed_tasks = []
 
+    for item in todo_data:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if item.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
+            completed_tasks.append(item.get("title"))
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for task in completed_tasks:
-        print("\t {}".format(task['title']))
+        print("\t {}".format(task))
+
+
+if __name__ == "__main__":
+    gather_api_data()
